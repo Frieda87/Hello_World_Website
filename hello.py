7c1ce94636e769de30b382f5f82932835d14b0a7
Friederike Land <friederikeland@friederikesair2.corp.ad.zalando.net>
2019-03-11 21:09:41 +0100

    pls revise my code

---
+++ hello.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Another attempt!!'
+
+@app.route('/restaurants')
+def get_restaurants():
+    return jsonify(restaurants)
+
+restaurants = [
+    {
+        'id': 1,
+        'name': '1990 Vegan living',
+        'address': 'Krossener Str. 19, 10245 Berlin', 
+        'food_type': 'vegan',
+        'google_reviews': 4.6,
+        'foodora_delivery': True
+    },
+    {
+        'id': 2,
+        'name': 'Chay Village',
+        'address': 'Niederbarnimstraße 10, 10249 Berlin', 
+        'food_type': 'vegetarian',
+        'google_reviews': 4.5,
+        'foodora_delivery': True
+    },
+    {
+        'id': 3,
+        'name': 'Gotcha',
+        'address': 'Simon-Dach-Straße 6, 10245 Berlin', 
+        'food_type': 'vietnamese',
+        'google_reviews': 4.4,
+        'foodora_delivery': True
+    },
+    {
+        'id': 4,
+        'name': 'Portofino',
+        'address': 'Gubener Str. 48, 10243 Berlin', 
+        'food_type': 'italian',
+        'google_reviews': 4.6,
+        'foodora_delivery': False
+    },
+    {
+        'id': 5,
+        'name': 'Speisehaus Berlin',
+        'address': 'Wühlischstraße 30, 10245 Berlin', 
+        'food_type': 'german',
+        'google_reviews': 4.3,
+        'foodora_delivery': False
+    }
+]
